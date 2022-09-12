7026a884535d735cd0570d26121c99694c147a3e
José Uscanga <jousmo@outlook.com>
2022-09-12 15:22:06 -0500

    feat: products mock data

--- src/db/mock.db.ts
+++ src/db/mock.db.ts
@@ -6,3 +6,22 @@ export const users = [
     role: 'admin',
   },
 ];
+
+export const products = [
+  {
+    id: 1,
+    name: 'Pepsi',
+    description: 'Refresco de cola',
+    price: 15,
+    stock: 100,
+    image: 'https://picsum.photos/200/300',
+  },
+  {
+    id: 2,
+    name: 'Coca Cola',
+    description: 'Refresco de cola',
+    price: 16,
+    stock: 100,
+    image: 'https://picsum.photos/200/300',
+  },
+];
