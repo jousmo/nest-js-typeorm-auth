f870d82183fc86b72fc679f129ba2e369ba8ba8c
José Uscanga <jousmo@outlook.com>
2022-09-12 14:56:04 -0500

    feat: update dto for user

--- src/users/dtos/create-user.dto.ts
+++ src/users/dtos/create-user.dto.ts
@@ -1,1 +1,15 @@
-export class CreateUserDto {}
+import { IsString, IsNotEmpty, IsEmail, Length } from 'class-validator';
+
+export class CreateUserDto {
+  @IsEmail()
+  readonly email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @Length(6)
+  readonly password: string;
+
+  @IsString()
+  @IsNotEmpty()
+  readonly role: string;
+}
