554a5742d7e1cf53b39f2af834a9134e84911f8e
José Uscanga <jousmo@outlook.com>
2022-09-13 21:09:18 -0500

    feat: validation type env file with joi

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
+
 import { UsersModule } from './users/users.module';
 import { ProductsModule } from './products/products.module';
 import { GlobalsModule } from './globals/globals.module';
@@ -11,6 +13,11 @@ import configuration from './configuration';
       envFilePath: '.env',
       load: [configuration],
       isGlobal: true,
+      validationSchema: Joi.object({
+        API_KEY: Joi.string().required(),
+        DB_NAME: Joi.string().required(),
+        DB_PORT: Joi.number().required(),
+      }),
     }),
     GlobalsModule,
     UsersModule,
