94e444f0591639d6993eb0615eee106cf7a40934
José Uscanga <jousmo@outlook.com>
2022-09-26 19:48:15 -0500

    feat: create provide factory with connection pg

--- src/globals/globals.module.ts
+++ src/globals/globals.module.ts
@@ -1,9 +1,26 @@
 import { Module, Global } from '@nestjs/common';
+import { ConfigType } from '@nestjs/config';
+import { Client } from 'pg';
+import configuration from '../configuration';
 
 @Global()
 @Module({
   imports: [],
-  providers: [],
-  exports: [],
+  providers: [
+    {
+      provide: 'PG',
+      useFactory: async (
+        configurationService: ConfigType<typeof configuration>,
+      ) => {
+        const { postgres } = configurationService;
+        const client = new Client(postgres);
+        await client.connect();
+        console.log('Connected Database');
+        return client;
+      },
+      inject: [configuration.KEY],
+    },
+  ],
+  exports: ['PG'],
 })
 export class GlobalsModule {}
