4e5f20d4c9ac5588f47cd97af2700a1139a19427
José Uscanga <jousmo@outlook.com>
2022-09-26 22:24:11 -0500

    feat: create entity table product with typeorm

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,8 +1,22 @@
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+@Entity()
 export class Product {
+  @PrimaryGeneratedColumn()
   id: number;
+
+  @Column({ type: 'varchar', length: 255, unique: true })
   name: string;
+
+  @Column({ type: 'text' })
   description: string;
+
+  @Column({ type: 'float', default: 0 })
   price: number;
+
+  @Column({ type: 'int', default: 0 })
   stock: number;
+
+  @Column({ type: 'varchar', length: 255 })
   image: string;
 }
