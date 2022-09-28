7bf71c5b6e280ec331df2443576601124ba67a05
José Uscanga <jousmo@outlook.com>
2022-09-28 14:51:46 -0500

    feat: create entities for customer and user for typeorm

--- src/users/entities/customer.entity.ts
+++ src/users/entities/customer.entity.ts
@@ -1,6 +1,17 @@
-export class Customer {
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+import { BaseEntity } from '../../globals/entities/base.entity';
+
+@Entity()
+export class Customer extends BaseEntity {
+  @PrimaryGeneratedColumn()
   id: number;
+
+  @Column({ type: 'varchar', length: 255 })
   name: string;
+
+  @Column({ type: 'varchar', length: 255 })
   lastName: string;
+
+  @Column({ type: 'varchar', length: 255 })
   phone: string;
 }
--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,6 +1,17 @@
-export class User {
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+import { BaseEntity } from '../../globals/entities/base.entity';
+
+@Entity()
+export class User extends BaseEntity {
+  @PrimaryGeneratedColumn()
   id: number;
+
+  @Column({ type: 'varchar', length: 255 })
   email: string;
+
+  @Column({ type: 'varchar', length: 255 })
   password: string;
+
+  @Column({ type: 'varchar', length: 100 })
   role: string;
 }
