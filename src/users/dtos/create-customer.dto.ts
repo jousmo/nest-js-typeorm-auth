c6f9d75ed293cac56e7e78f4aec6c833923054c4
José Uscanga <jousmo@outlook.com>
2022-09-28 14:52:15 -0500

    feat: create dto for customer

---
+++ src/users/dtos/create-customer.dto.ts
@@ -0,0 +1,15 @@
+import { IsString, IsNotEmpty, IsPhoneNumber } from 'class-validator';
+
+export class CreateCustomerDto {
+  @IsString()
+  @IsNotEmpty()
+  readonly name: string;
+
+  @IsString()
+  @IsNotEmpty()
+  readonly lastName: string;
+
+  @IsPhoneNumber()
+  @IsNotEmpty()
+  readonly phone: string;
+}
