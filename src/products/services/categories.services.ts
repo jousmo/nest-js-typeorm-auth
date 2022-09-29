85c6612decd6469d4dabed309de13e218e68d51d
José Uscanga <jousmo@outlook.com>
2022-09-29 15:15:40 -0500

    feat: implement services for categories

---
+++ src/products/services/categories.services.ts
@@ -0,0 +1,47 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { DeleteResult, Repository } from 'typeorm';
+import { CreateCategoryDto } from '../dtos/create-category.dto';
+import { UpdateCategoryDto } from '../dtos/update-category.dto';
+import { Category } from '../entities/category.entity';
+
+@Injectable()
+export class CategoriesService {
+  constructor(
+    @InjectRepository(Category)
+    private categoryRepository: Repository<Category>,
+  ) {}
+
+  create(createCategoryDto: CreateCategoryDto): Promise<Category> {
+    const newCategory = this.categoryRepository.create(createCategoryDto);
+    return this.categoryRepository.save(newCategory);
+  }
+
+  findAll(): Promise<Category[]> {
+    return this.categoryRepository.find();
+  }
+
+  async findOne(id: number): Promise<Category> {
+    const category = await this.categoryRepository.findOne({
+      where: { id },
+    });
+    if (!category) {
+      throw new NotFoundException(`Category #${id} not found`);
+    }
+    return category;
+  }
+
+  async update(
+    id: number,
+    updateCategoryDto: UpdateCategoryDto,
+  ): Promise<Category> {
+    const category = await this.findOne(id);
+    this.categoryRepository.merge(category, updateCategoryDto);
+    return this.categoryRepository.save(category);
+  }
+
+  async remove(id: number): Promise<DeleteResult> {
+    await this.findOne(id);
+    return this.categoryRepository.delete(id);
+  }
+}
