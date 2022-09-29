cd63a4418fd547a698d8da0b1a11488a40d4b64d
José Uscanga <jousmo@outlook.com>
2022-09-28 22:13:04 -0500

    feat: implement controller and services for brand

---
+++ src/products/services/brands.service.ts
@@ -0,0 +1,41 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { DeleteResult, Repository } from 'typeorm';
+import { CreateBrandDto } from '../dtos/create-brand.dto';
+import { UpdateBrandDto } from '../dtos/update-brand.dto';
+import { Brand } from '../entities/brand.entity';
+
+@Injectable()
+export class BrandsService {
+  constructor(
+    @InjectRepository(Brand) private brandRepository: Repository<Brand>,
+  ) {}
+
+  create(createBrandDto: CreateBrandDto): Promise<Brand> {
+    const newBrand = this.brandRepository.create(createBrandDto);
+    return this.brandRepository.save(newBrand);
+  }
+
+  findAll(): Promise<Brand[]> {
+    return this.brandRepository.find();
+  }
+
+  async findOne(id: number): Promise<Brand> {
+    const brand = await this.brandRepository.findOne({ where: { id } });
+    if (!brand) {
+      throw new NotFoundException(`Brand #${id} not found`);
+    }
+    return brand;
+  }
+
+  async update(id: number, updateBrandDto: UpdateBrandDto): Promise<Brand> {
+    const brand = await this.findOne(id);
+    this.brandRepository.merge(brand, updateBrandDto);
+    return this.brandRepository.save(brand);
+  }
+
+  async remove(id: number): Promise<DeleteResult> {
+    await this.findOne(id);
+    return this.brandRepository.delete(id);
+  }
+}
