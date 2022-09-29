f71b8abf5f8de77544f6b258cdec90b2da0bfe9c
José Uscanga <jousmo@outlook.com>
2022-09-29 15:16:04 -0500

    feat: add logic controller and services in module product for categories

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -4,13 +4,16 @@ import { ProductsService } from './services/products.service';
 import { ProductsController } from './controllers/products.controller';
 import { Product } from './entities/product.entity';
 import { Brand } from './entities/brand.entity';
+import { Category } from './entities/category.entity';
 import { BrandsController } from './controllers/brands.controller';
 import { BrandsService } from './services/brands.service';
+import { CategoriesService } from './services/categories.services';
+import { CategoriesController } from './controllers/categories.controller';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Product, Brand])],
-  controllers: [ProductsController, BrandsController],
-  providers: [ProductsService, BrandsService],
+  imports: [TypeOrmModule.forFeature([Product, Brand, Category])],
+  controllers: [ProductsController, BrandsController, CategoriesController],
+  providers: [ProductsService, BrandsService, CategoriesService],
   exports: [ProductsService],
 })
 export class ProductsModule {}
