9828dbfde414a6e14f2e073bb444f1751907d3e1
José Uscanga <jousmo@outlook.com>
2022-10-01 15:01:08 -0500

    feat: implement transform data serialize with expose new data and exclude

--- src/users/entities/order.entity.ts
+++ src/users/entities/order.entity.ts
@@ -1,4 +1,5 @@
 import { Entity, ManyToOne, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+import { Exclude, Expose } from 'class-transformer';
 import { BaseEntity } from '../../globals/entities/base.entity';
 import { Customer } from './customer.entity';
 import { OrderItem } from './order-item.entity';
@@ -11,6 +12,22 @@ export class Order extends BaseEntity {
   @ManyToOne(() => Customer, (customer) => customer.orders)
   customer: Customer;
 
+  @Exclude()
   @OneToMany(() => OrderItem, (item) => item.order)
   items: OrderItem[];
+
+  @Expose()
+  get products() {
+    if (this.items) {
+      return this.items
+        .filter((item) => !!item)
+        .map((item) => ({
+          ...item.product,
+          quantity: item.quantity,
+          itemId: item.id,
+        }));
+    }
+
+    return [];
+  }
 }
