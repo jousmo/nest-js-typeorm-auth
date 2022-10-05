e905f02c841e9f102bac8561c6f7311bf530fe55
José Uscanga <jousmo@outlook.com>
2022-10-05 11:57:42 -0500

    feat: logic for generate token with jwt

--- src/auth/services/auth.service.ts
+++ src/auth/services/auth.service.ts
@@ -1,11 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
 import { User } from '../../users/entities/user.entity';
 import { UsersService } from '../../users/services/users.service';
+import { PayloadTokenModel } from '../models/token.model';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly usersService: UsersService) {}
+  constructor(
+    private readonly usersService: UsersService,
+    private readonly jwtService: JwtService,
+  ) {}
 
   async validateUser(email: string, password: string): Promise<User> {
     const user = await this.usersService.findByEmail(email);
@@ -15,5 +20,13 @@ export class AuthService {
     const isMatch = await bcrypt.compare(password, user.password);
 
     return isMatch ? user : null;
+  }
+
+  async generateJWT(user: User) {
+    const payload: PayloadTokenModel = { role: user.role, sub: user.id };
+    return {
+      access_token: this.jwtService.sign(payload),
+      user,
+    };
   }
 }
