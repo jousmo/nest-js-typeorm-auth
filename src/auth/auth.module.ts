f02882be11c9e3d5168dc87e2fcba91421b0df65
José Uscanga <jousmo@outlook.com>
2022-10-05 11:58:00 -0500

    feat: implement jwt in module and controller

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,12 +1,27 @@
 import { Module } from '@nestjs/common';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigType } from '@nestjs/config';
 import { AuthService } from './services/auth.service';
 import { UsersModule } from '../users/users.module';
-import { PassportModule } from '@nestjs/passport';
 import { LocalStrategy } from './strategies/local.strategy';
 import { AuthController } from './controllers/auth.controller';
+import configuration from '../configuration';
 
 @Module({
-  imports: [UsersModule, PassportModule],
+  imports: [
+    UsersModule,
+    PassportModule,
+    JwtModule.registerAsync({
+      inject: [configuration.KEY],
+      useFactory: (configurationService: ConfigType<typeof configuration>) => {
+        return {
+          secret: configurationService.jwtSecret,
+          signOptions: { expiresIn: configurationService.jwtExpired },
+        };
+      },
+    }),
+  ],
   providers: [AuthService, LocalStrategy],
   controllers: [AuthController],
 })
--- src/auth/controllers/auth.controller.ts
+++ src/auth/controllers/auth.controller.ts
@@ -1,12 +1,15 @@
 import { Controller, Request, Post, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { User } from '../../users/entities/user.entity';
+import { AuthService } from '../services/auth.service';
 
 @Controller('auth')
 export class AuthController {
+  constructor(private readonly authService: AuthService) {}
   @UseGuards(AuthGuard('local'))
   @Post('/login')
-  async login(@Request() req): Promise<User> {
-    return req.user;
+  async login(@Request() req): Promise<any> {
+    const user = req.user as User;
+    return this.authService.generateJWT(user);
   }
 }
